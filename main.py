f25f31007fa76144cf26b9434c27d5fee9202183
SlalomSquid <slalomsquid@outlook.com>
2026-04-24 20:15:24 +0800

    Created gitignore and removed pycache

--- main.py
+++ main.py
@@ -9,15 +9,28 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Platformer Example")
 
 class Block():
-    def __init__(self, x, y, sx, sy):
+    def __init__(self, x, y, sx, sy, color=(0, 255, 255), texture=None):
         super().__init__()
         self.x = x
         self.y = y
         self.sx = sx
         self.sy = sy
-        self.color = (0, 255, 255)
+        self.color = color
+        self.texture = texture
+
+class Player():
+    def __init__(self, x, y, sx, sy, color=(255, 50, 50), texture=None):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.texture = texture
 
 blocks = [Block(100, 100, 50, 50), Block(200, 150, 50, 50)]
+
+player = Player(ORIGIN[0], ORIGIN[1], 20, 20)
 
 running = True
 
@@ -50,5 +63,7 @@ while running:
 
     for block in blocks:
         pygame.draw.rect(screen, block.color, (block.x, block.y, block.sx, block.sy))
+
+    pygame.draw.rect(screen, player.color, (player.x, player.y, player.sx, player.sy))
 
     pygame.display.flip()
